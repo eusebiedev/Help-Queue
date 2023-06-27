707b87e4ace0dc81d64e65c5e50e25cf318fd50e
Eusebie Siebenberg <siebenee@gmail.com>
2023-06-27 11:37:20 -0700

    add ConfirmSteps.js for ticketcontrol.js if branch

---
+++ src/components/ConfirmSteps.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ConfirmSteps(){
+  return (
+
+      <h1>Have you gone through all the steps on the Learn How to Program debugging lesson?</h1>
+  );
+}
+
+export default ConfirmSteps; 
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
+import ConfirmSteps from './ConfirmSteps';
 
 class TicketControl extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      confirmSteps: false
     };
   }
 
   handleClick = () => {
-    this.setState(prevState => ({
+    if (this.state.confirmSteps === false) {
+      this.setState(prevState => ({
+      confirmSteps: !prevState.confirmSteps 
+      }));
+    } else if (this.state.formVisibleOnPage === false) {
+      this.setState(prevState => ({
       formVisibleOnPage: !prevState.formVisibleOnPage
-    }));
+      }));
+    } else { 
+      this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage,
+      confirmSteps: !prevState.confirmSteps 
+      }));
+    }
   }
-
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm />
       buttonText = "Return to Ticket List";
-    } else {
+    } else if (this.state.confirmSteps) {
+      currentlyVisibleState = <ConfirmSteps />
+      buttonText = "Yes";
+    } else 
       currentlyVisibleState = <TicketList />
       buttonText = "Add Ticket";
-    }
+    
     return(
       <React.Fragment>
         {currentlyVisibleState}
