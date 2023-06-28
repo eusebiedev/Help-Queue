c6bd3e2fce9ae1680f7a268921042c51ee2d130a
Eusebie Siebenberg <siebenee@gmail.com>
2023-06-28 09:40:37 -0700

    Add UUID, Form to project
    Co-authored-by: Joseph Wilfong <jcarenza67@users.noreply.github.com>

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
-import ConfirmSteps from './ConfirmSteps';
+// import ConfirmSteps from './ConfirmSteps';
 
 class TicketControl extends React.Component {
 
@@ -9,46 +9,79 @@ class TicketControl extends React.Component {
     super(props);
     this.state = {
       formVisibleOnPage: false,
-      confirmSteps: false
+      // confirmSteps: false
+      mainTicketList: []
     };
   }
 
   handleClick = () => {
-    if (this.state.confirmSteps === false) {
-      this.setState(prevState => ({
-      confirmSteps: !prevState.confirmSteps 
-      }));
-    } else if (this.state.formVisibleOnPage === false) {
-      this.setState(prevState => ({
+    this.setState(prevState => ({
       formVisibleOnPage: !prevState.formVisibleOnPage
-      }));
-    } else { 
-      this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage,
-      confirmSteps: !prevState.confirmSteps 
-      }));
-    }
+    }));
   }
+
+  handleAddingNewTicketToList = (newTicket) => {
+    const newMainTicketList = this.state.mainTicketList.concat(newTicket);
+    this.setState({mainTicketList: newMainTicketList, 
+      formVisibleOnPage: false
+    });
+  }
+
   render(){
     let currentlyVisibleState = null;
-    let buttonText = null;
+    let buttonText = null; 
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />
-      buttonText = "Return to Ticket List";
-    } else if (this.state.confirmSteps) {
-      currentlyVisibleState = <ConfirmSteps />
-      buttonText = "Yes";
-    } else 
-      currentlyVisibleState = <TicketList />
-      buttonText = "Add Ticket";
-    
-    return(
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation
+      ={this.handleAddingNewTicketToList}/>;
+      buttonText = "Return to Ticket List"; 
+    } else {
+      currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList}/>;
+      buttonText = "Add Ticket"; 
+    }
+    return (
       <React.Fragment>
         {currentlyVisibleState}
-        <button onClick={this.handleClick}>{buttonText}</button>
+        <button onClick={this.handleClick}>{buttonText}</button> 
       </React.Fragment>
     );
   }
+
+    // handleClick = () => {
+  //   if (this.state.confirmSteps === false) {
+  //     this.setState(prevState => ({
+  //     confirmSteps: !prevState.confirmSteps 
+  //     }));
+  //   } else if (this.state.formVisibleOnPage === false) {
+  //     this.setState(prevState => ({
+  //     formVisibleOnPage: !prevState.formVisibleOnPage
+  //     }));
+  //   } else { 
+  //     this.setState(prevState => ({
+  //     formVisibleOnPage: !prevState.formVisibleOnPage,
+  //     confirmSteps: !prevState.confirmSteps 
+  //     }));
+  //   }
+  // }
+  // render(){
+  //   let currentlyVisibleState = null;
+  //   let buttonText = null;
+  //   if (this.state.formVisibleOnPage) {
+  //     currentlyVisibleState = <NewTicketForm />
+  //     buttonText = "Return to Ticket List";
+  //   } else if (this.state.confirmSteps) {
+  //     currentlyVisibleState = <ConfirmSteps />
+  //     buttonText = "Yes";
+  //   } else 
+  //     currentlyVisibleState = <TicketList />
+  //     buttonText = "Add Ticket";
+    
+  //   return(
+  //     <React.Fragment>
+  //       {currentlyVisibleState}
+  //       <button onClick={this.handleClick}>{buttonText}</button>
+  //     </React.Fragment>
+  //   );
+  // }
 }
 
 export default TicketControl
